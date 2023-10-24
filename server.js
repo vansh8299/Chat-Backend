81f4ef53b59cdcc9f1e2560689d95e8d97ded59b
vansh8299 <76584561+vansh8299@users.noreply.github.com>
2023-10-24 19:28:45 +0530

    update

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-frontend-9agh.onrender.com",
   },
 });
 
